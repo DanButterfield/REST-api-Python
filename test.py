494605ce127fe1a604e0e4f9b45de8345430240a
Dan <c3020828@newcastle.ac.uk>
2024-10-04 11:56:06 +0100

    ADD patch testing Commented out other testing

--- test.py
+++ test.py
@@ -1,7 +1,9 @@
 import requests
 
 BASE = "http://127.0.0.1:5000/"
-
+response = requests.patch(BASE + "video/2", {"views = 99", "likes=101"})
+print(response)
+'''
 data = [{"likes": 90, "name": "How to Cook", "views": 2000}, {"likes": 20, "name": "How to Code", "views": 230},
         {"likes": 12340, "name": "How to Clean", "views": 5000}, {"likes": 6, "name": "How to Garden", "views": 4000},
         {"likes": 900, "name": "How to drive", "views": 60}]
@@ -11,8 +13,8 @@ for i in range(len(data)):
     print(response.json())
 
 input()
-response = requests.delete(BASE + "video/0")
-print(response)
-input()
-response = requests.get(BASE + "video/6")
+response = requests.patch(BASE + "video/7")
 print(response.json())
+'''
+
+
